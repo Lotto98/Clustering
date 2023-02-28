9eea3bb5fd6513323328f2164eb4f71882e84032
MICHELE LOTTO <875922@stud.unive.it>
2023-02-28 19:33:16 +0100

    Better data sampling Added plot of digits for each cluster

--- utility.py
+++ utility.py
@@ -113,11 +113,25 @@ def get_results(dfs:dict,
         
     return results, best_indexes, fitted_estimators
 
-def plot_clustering(X, labels, cluster_centers=None):
-
-    labels_unique = np.unique(labels)
-    n_clusters_ = len(labels_unique)
+def best_pca_dimension(results:dict,best_indexes:dict,hyperparameter_name:str):
     
+    max_=0
+    best_pca=None
+
+    for x in range(2,200,10):
+        s=results[x].iloc[best_indexes[x]]
+        hyperparameter=s[hyperparameter_name]
+        rand_index=s["rand index"]
+        print(f"for PCA dimension {x}, the best {hyperparameter_name} hyperparameter is { hyperparameter } with a rand score of {rand_index}")
+        
+        if rand_index>max_:
+            max_=rand_index
+            best_pca=x
+
+    print(f"\nThe best PCA dimension is {best_pca} with a rand score of {max_}")
+
+def plot_clustering(X, labels, n_clusters_, cluster_centers=None):
+
     plt.figure(figsize=(20,10))
     plt.clf()
 
@@ -131,7 +145,63 @@ def plot_clustering(X, labels, cluster_centers=None):
         
         if cluster_centers is not None:
             cluster_center = cluster_centers[k]
-            plt.scatter(cluster_center[0],cluster_center[1],marker=markers[k%len(markers)], edgecolor="black", s=150, color =col)
+            plt.scatter(cluster_center[0],cluster_center[1],marker=markers[k%len(markers)], edgecolor="black", s=200, color =col)
         
     plt.title("Estimated number of clusters: %d" % n_clusters_)
     plt.show()
+    
+def plot_images_per_cluster(type:str, dfs, fitted_estimators):
+    
+    for key in dfs.keys():
+        
+        labels=fitted_estimators[key].predict(dfs[key])
+        
+        if type=="GaussianMixture":
+            n_clusters_=fitted_estimators[key].get_params()["n_components"]
+        
+        if type=="normalize_cut":
+            n_clusters_=fitted_estimators[key].get_params()["n_clusters"]
+        
+        if type=="mean_shift":
+            labels_unique = np.unique(labels)
+            n_clusters_ = len(labels_unique)
+    
+        fig,axs=plt.subplots(n_clusters_,4 ,figsize=(4*3,n_clusters_*3))
+    
+        fig.suptitle(f"Digits plot for PCA dimension {key} with {n_clusters_} clusters")
+    
+        for item in [item for sublist in axs for item in sublist]:
+            item.set_yticklabels([])
+            item.set_xticklabels([])
+            
+        with open("pca/pca_"+str(key)+".pkl", 'rb') as inp:
+            pca=pickle.load(inp)
+            
+        for k in range(n_clusters_):
+            my_members = labels == k
+            
+            data=pca.inverse_transform(dfs[key][my_members])
+        
+            if data.shape[0]>=4:
+                random_indexes=np.random.choice(data.shape[0], size=4, replace=False)
+            else:
+                #print("si")
+                random_indexes=np.random.choice(data.shape[0], size=data.shape[0], replace=False)
+                
+            for i,imag in enumerate(data[random_indexes, :]):
+                    axs[k,i].imshow(imag.reshape(28, 28))
+                    
+            if data.shape[0]==0:
+                i=-1
+        
+            for j in range(i+1,4):
+                axs[k,j].imshow(np.zeros(28*28).reshape(28,28))
+                
+        for ax,c in zip(axs[:,0],range(n_clusters_)):
+            ax.set_ylabel(str(c), rotation=0, size='large')
+        
+        fig.tight_layout()
+        fig.subplots_adjust(top=0.95)
+        fig.show()
+                
+    
